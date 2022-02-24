1e80949a433d399ece500e888d1ba38dcd67c881
ChandlerWill <41649311+ChandlerWill@users.noreply.github.com>
2022-02-24 16:21:02 -0500

    Initial commit
    Basic pages, starter code

---
+++ src/pages/projects/pdgaStats.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default function pdgaStats() {
+  return (
+    <Layout>
+        <div>pdgaStats</div>
+    </Layout>
+  )
+}
